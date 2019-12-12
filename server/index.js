3d16ba0d65b76c0bfb1148da86d334774adcc074
Inna Nikolenko <innaivasyuk93@gmail.com>
2019-12-11 16:59:12 -0800

    Serving four/five apps

--- server/index.js
+++ server/index.js
@@ -17,40 +17,58 @@ app.use(express.json());
 // app.get('*', ((req, res) => {
 //   request(requestMap[req.path.url], (err, response) => res.send(response.body));
 // }))
-// app.get('/kyle-service/bundle.js', (req, res) => {
-//   request('http://localhost:3001/bundle.js', (err, response) => {
-//     res.send(response.body);
-//   });
-// });
 
-// app.get('/api/listings_kyle/:Listing_id', (req, res) => {
-//   request('http://localhost:3001/api/listings_kyle/' + req.params.Listing_id, (err, response) => {
-//     res.send(response.body);
-//   });
-// });
+// docker run -d -p 3001:3001 -- kyle
+// docker run -d -p 3002:3002 -- patrick
+// docker run -d -p 3003:3003 -- sayer
+// docker run -d -p 3004:3004 -- matt
+// docker run -d -p 3005:3005 -- inna
 
-// app.get('/patrick-service/bundle.js', (req, res) => {
-//   request('http://localhost:3002/bundle.js', (err, response) => {
-//     if (err) {
-//       console.log(err);
-//     }
-//     res.send(response.body);
-//   });
-// });
+app.get('/kyle-service/bundle.js', (req, res) => {
+  request('http://localhost:3001/bundle.js', (err, response) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+    res.send(response.body);
+  });
+});
 
-// app.get('/listings/:id', (req, res) => {
-//   request('http://localhost:3002/listings/' + req.params.id, (err, response) => {
-//     if (err) {
-//       console.log(err);
-//     }
-//     res.send(response.body);
-//   });
-// });
+app.get('/api/listings/:Listing_id', (req, res) => {
+  request('http://localhost:3001/api/listings/' + req.params.Listing_id, (err, response) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+    res.send(response.body);
+  });
+});
+
+app.get('/patrick-service/bundle.js', (req, res) => {
+  request('http://localhost:3002/bundle.js', (err, response) => {
+if (err) {
+  console.log(err);
+  res.sendStatus(500);
+}
+    res.send(response.body);
+  });
+});
+
+app.get('/listings/:id', (req, res) => {
+  request('http://localhost:3002/listings/' + req.params.id, (err, response) => {
+if (err) {
+  console.log(err);
+  res.sendStatus(500);
+}
+    res.send(response.body);
+  });
+});
 
 app.get('/sayer-service/bundle.js', (req, res) => {
   request('http://localhost:3003/bundle.js', (err, response) => {
     if (err) {
       console.log(err);
+      res.sendStatus(500);
     }
     res.send(response.body);
   });
@@ -60,6 +78,7 @@ app.get('/api/listing/:Listing_id/', (req, res) => {
   request('http://localhost:3003/api/listing/' + req.params.Listing_id, (err, response) => {
     if (err) {
       console.log(err);
+      res.sendStatus(500);
     }
     res.send(response.body);
   });
@@ -67,12 +86,20 @@ app.get('/api/listing/:Listing_id/', (req, res) => {
 
 // app.get('/matt-service/bundle.js', (req, res) => {
 //   request('http://localhost:3004/bundle.js', (err, response) => {
+// if (err) {
+//   console.log(err);
+//   res.sendStatus(500);
+// }
 //     res.send(response.body);
 //   });
 // });
 
 // app.get('/api/listings/:listing_id/', (req, res) => {
 //   request('http://localhost:3004/api/listings/' + req.params.listing_id, (err, response) => {
+// if (err) {
+//   console.log(err);
+//   res.sendStatus(500);
+// }
 //     res.send(response.body);
 //   });
 // });
@@ -83,6 +110,7 @@ app.get('/inna-service/bundle.js', (req, res) => {
 
     if (err) {
       console.log(err);
+      res.sendStatus(500);
     }
     res.send(response.body);
   });
@@ -92,6 +120,7 @@ app.get('/api/photos/:id/', (req, res) => {
   request('http://localhost:3005/api/photos/' + req.params.id, (err, response) => {
     if (err) {
       console.log(err);
+      res.sendStatus(500);
     }
 
     res.send(response.body);
