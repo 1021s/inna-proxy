c7883a307cf73496ecb5a7d4ad38c6d64c5de11f
Inna Nikolenko <innaivasyuk93@gmail.com>
2019-12-13 11:21:15 -0800

    Styled full page

--- server/index.js
+++ server/index.js
@@ -9,118 +9,99 @@ const PORT = 3000 || process.env.PORT;
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
 app.use(express.json());
 
-// const requestMap = {
-//   'inna-service/bundle.js': 'http://localhost:3005/dist/bundle.js'
-//   'inna-service/bundle.css': 'http://localhost:3005/dist/bundle.css'
-// }
+// // docker run -d -p 3001:3001 -- kyle
+// // docker run -d -p 3002:3002 -- patrick
+// // docker run -d -p 3003:3003 -- sayer
+// // docker run -d -p 3004:3004 -- matt
+// // docker run -d -p 3005:3005 -- inna
 
-// app.get('*', ((req, res) => {
-//   request(requestMap[req.path.url], (err, response) => res.send(response.body));
-// }))
-
-// docker run -d -p 3001:3001 -- kyle
-// docker run -d -p 3002:3002 -- patrick
-// docker run -d -p 3003:3003 -- sayer
-// docker run -d -p 3004:3004 -- matt
-// docker run -d -p 3005:3005 -- inna
+// const kyle = '34.213.221.210';
+// const patrick = '18.236.158.99';
+// const sayer = '18.221.25.146';
+// const matt = '54.202.99.13';
+// const inna = '35.164.99.110';
 
 app.get('/kyle-service/bundle.js', (req, res) => {
-  request('http://localhost:3001/bundle.js', (err, response) => {
-    if (err) {
-      console.log(err);
-      res.sendStatus(500);
-    }
+  request('http://54.213.38.65:3001/main.bundle.js', (err, response) => {
+    res.send(response.body);
+  });
+});
+
+app.get('/kyle-service/vendors.js', (req, res) => {
+  request('http://54.213.38.65:3001/vendors.bundle.js', (err, response) => {
     res.send(response.body);
   });
 });
 
 app.get('/api/listings/:Listing_id', (req, res) => {
-  request('http://localhost:3001/api/listings/' + req.params.Listing_id, (err, response) => {
-    if (err) {
-      console.log(err);
-      res.sendStatus(500);
-    }
+  request('http://54.213.38.65:3001/api/listings/' + req.params.Listing_id, (err, response) => {
     res.send(response.body);
   });
 });
 
 app.get('/patrick-service/bundle.js', (req, res) => {
-  request('http://localhost:3002/bundle.js', (err, response) => {
-if (err) {
-  console.log(err);
-  res.sendStatus(500);
-}
+  request('http://54.213.192.23:3002/bundle.js', (err, response) => {
+    if (err) {
+      console.log(err);
+    }
     res.send(response.body);
   });
 });
 
 app.get('/listings/:id', (req, res) => {
-  request('http://localhost:3002/listings/' + req.params.id, (err, response) => {
-if (err) {
-  console.log(err);
-  res.sendStatus(500);
-}
+  request('http://54.213.192.23:3002/listings/' + req.params.id, (err, response) => {
+    if (err) {
+      console.log(err);
+    }
     res.send(response.body);
   });
 });
 
 app.get('/sayer-service/bundle.js', (req, res) => {
-  request('http://localhost:3003/bundle.js', (err, response) => {
+  request('http://18.191.248.186:3003/bundle.js', (err, response) => {
     if (err) {
       console.log(err);
-      res.sendStatus(500);
     }
     res.send(response.body);
   });
 });
 
 app.get('/api/listing/:Listing_id/', (req, res) => {
-  request('http://localhost:3003/api/listing/' + req.params.Listing_id, (err, response) => {
+  request('http://18.191.248.186:3003/api/listing/' + req.params.Listing_id, (err, response) => {
     if (err) {
       console.log(err);
-      res.sendStatus(500);
     }
     res.send(response.body);
   });
 });
 
-// app.get('/matt-service/bundle.js', (req, res) => {
-//   request('http://localhost:3004/bundle.js', (err, response) => {
-// if (err) {
-//   console.log(err);
-//   res.sendStatus(500);
-// }
-//     res.send(response.body);
-//   });
-// });
+app.get('/matt-service/bundle.js', (req, res) => {
+  request('http://18.237.82.132:3004/mc.bundle.js', (err, response) => {
+    res.send(response.body);
+  });
+});
 
-// app.get('/api/listings/:listing_id/', (req, res) => {
-//   request('http://localhost:3004/api/listings/' + req.params.listing_id, (err, response) => {
-// if (err) {
-//   console.log(err);
-//   res.sendStatus(500);
-// }
-//     res.send(response.body);
-//   });
-// });
+app.get('/api/houseprices/:listing_id/', (req, res) => {
+  request('http://18.237.82.132:3004/api/houseprices/' + req.params.listing_id, (err, response) => {
+    res.send(response.body);
+  });
+});
 
 app.get('/inna-service/bundle.js', (req, res) => {
-  request('http://localhost:3005/dist/bundle.js', (err, response) => {
+  request('http://35.164.99.110:3005/dist/bundle.js', (err, response) => {
     // console.log('response ', response.body);
 
     if (err) {
       console.log(err);
-      res.sendStatus(500);
     }
     res.send(response.body);
   });
 });
 
 app.get('/api/photos/:id/', (req, res) => {
-  request('http://localhost:3005/api/photos/' + req.params.id, (err, response) => {
+  request('http://35.164.99.110:3005/api/photos/' + req.params.id, (err, response) => {
     if (err) {
       console.log(err);
-      res.sendStatus(500);
     }
 
     res.send(response.body);
